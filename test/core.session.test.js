2363998ea80a8b698db9297738e1b9ffb61b4c5d
Uditha Atukorala <udi@geniusse.com>
2015-04-20 21:02:21 +0100

    extending test coverage for session module

--- test/core.session.test.js
+++ test/core.session.test.js
@@ -43,6 +43,12 @@ describe( 'session', function() {
 
 	describe( 'start', function () {
 
+		var credentials = {
+			username : 'dummy',
+			password : 'pass'
+		};
+
+
 		it( 'should validate credentials', function ( done ) {
 			session.start( {}, function ( err, data ) {
 				expect( err ).to.be.an.instanceof( Error );
@@ -52,11 +58,6 @@ describe( 'session', function() {
 		} );
 
 		it( 'should make a login request', function ( done ) {
-			var credentials = {
-				username : 'dummy',
-				password : 'pass'
-			};
-
 			var scope = nock( session.endpoint() )
 				.post( '?svc=core/login' )
 				.reply( 200, { eid : 'cfdf5e9dc900991577c10e3934b6c8f0' } );
@@ -68,22 +69,47 @@ describe( 'session', function() {
 			} );
 		} );
 
+		context( 'when API credentials are incorrect', function () {
+			it( 'should return an error object', function ( done ) {
+				var scope = nock( session.endpoint() )
+					.post( '?svc=core/login' )
+					.reply( 200, { error : 8 } );
+
+				session.start( credentials, function ( err, sess ) {
+					expect( err ).to.be.an.instanceof( Error );
+					expect( err.message ).to.be.string( 'API error: 8' );
+					done();
+				} );
+			} );
+		} );
+
 	} );
 
 
 	describe( 'request', function () {
 
-		context( 'core/login', function() {
-			it( 'should call the endpoint url', function ( done ) {
-				var svc    = 'core/login';
-				var scope  = nock( session.endpoint() )
-					.post( '?svc=' + svc )
-					.reply( 200, { error : 0 } );
+		it( 'should call the endpoint url', function ( done ) {
+			var svc    = 'core/login';
+			var scope  = nock( session.endpoint() )
+				.post( '?svc=' + svc )
+				.reply( 200, { error : 0 } );
 
-				session.request( svc, {}, function ( err, data ) {
-					expect( scope.isDone() ).to.be.true;
-					done();
-				} );
+			session.request( svc, {}, function ( err, data ) {
+				expect( scope.isDone() ).to.be.true;
+				done();
+			} );
+		} );
+
+		it( 'should return API errors', function ( done ) {
+			var svc    = 'core/login';
+			var scope  = nock( session.endpoint() )
+				.post( '?svc=' + svc )
+				.reply( 200, { error : 8 } );
+
+			session.request( svc, {}, function ( err, data ) {
+				expect( err ).to.be.an.instanceof( Error );
+				expect( err.message ).to.be.string( 'API error: 8' );
+				done();
 			} );
 		} );
 
@@ -92,6 +118,38 @@ describe( 'session', function() {
 				expect( err ).to.be.an.instanceof( Error );
 				expect( err.message ).to.be.string( 'Invalid session' );
 				done();
+			} );
+		} );
+
+		context( 'when failed to reach the API endpoint', function () {
+			it( 'should return an error object', function ( done ) {
+				var svc    = 'core/login';
+				var scope  = nock( session.endpoint() )
+					.post( '?svc=' + svc )
+					.replyWithError( 'API request failed' );
+
+				session.request( svc, {}, function ( err, data ) {
+					expect( err ).to.be.an.instanceof( Error );
+					expect( err.message ).to.be.string( 'API request failed' );
+
+					// clear all nocks which seems to be needed after replyWithError()
+					nock.cleanAll();
+					done();
+				} );
+			} );
+		} );
+
+		context( 'when API response is not JSON', function () {
+			it( 'should return an error object', function ( done ) {
+				var svc    = 'core/login';
+				var scope  = nock( session.endpoint() )
+					.post( '?svc=' + svc )
+					.reply( 200, 'not JSON' );
+
+				session.request( svc, {}, function ( err, data ) {
+					expect( err ).to.be.an.instanceof( SyntaxError );
+					done();
+				} );
 			} );
 		} );
 
